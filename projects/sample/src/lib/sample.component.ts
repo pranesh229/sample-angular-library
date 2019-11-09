04cd0d2d33a51b36aa3fe9ae6e7b050dd6174342
Pranesh Hanumanth <praneshhanumanth@Praneshs-MacBook-Pro.local>
2019-11-09 15:47:32 -0500

    Sample library

---
+++ projects/sample/src/lib/sample.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-sample',
+  template: `
+    <p>
+      sample works!
+    </p>
+  `,
+  styles: []
+})
+export class SampleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/sample/src/lib/sample.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { SampleComponent } from './sample.component';
+
+
+
+@NgModule({
+  declarations: [SampleComponent],
+  imports: [
+  ],
+  exports: [SampleComponent]
+})
+export class SampleModule { }
